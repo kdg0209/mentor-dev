8d288e60da4d98d23cda208f41f7c0e6a4afbc61
gimdong-gyun <s_cova_0418@naver.com>
2022-02-28 09:56:45 +0900

    docker Exampple

---
+++ src/main/java/com/intw/mentorapi/admin/ExampleController.java
@@ -0,0 +1,25 @@
+package com.intw.mentorapi.admin;
+
+import com.intw.mentorapi.response.ApiResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "Example / example")
+@RequestMapping("/admin/example")
+@RestController
+@RequiredArgsConstructor
+public class ExampleController {
+
+//    sudo docker stop "sudo docker ps -q"
+//    sudo docker rmi --force "cova1256/dev"
+
+    @GetMapping("/lists")
+    @ApiOperation(value="test 목록")
+    public ApiResponse lists() {
+        return null;
+    }
+}
