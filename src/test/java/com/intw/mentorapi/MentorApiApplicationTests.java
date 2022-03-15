621e3be1759d422a0a0b5c1fe7ec75347b6ef523
gimdong-gyun <s_cova_0418@naver.com>
2022-03-15 13:40:32 +0900

    build profile test

--- src/test/java/com/intw/mentorapi/MentorApiApplicationTests.java
+++ src/test/java/com/intw/mentorapi/MentorApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.intw.mentorapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MentorApiApplicationTests {
 
     @Test
